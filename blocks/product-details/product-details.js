0afcbe071b1af76c8d98233e90f0a34b47c53fa7
Pankaj Garg <pagarg@adobe.com>
2025-06-03 17:37:35 +0530

    performance fix

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -77,6 +77,15 @@ export default async function decorate(block) {
   // Alert
   let inlineAlert = null;
 
+  // Preload first product image
+  if (product?.images?.[0]?.url) {
+    const link = document.createElement('link');
+    link.rel = 'preload';
+    link.as = 'image';
+    link.href = product.images[0].url;
+    document.head.appendChild(link);
+  }
+
   // Render Containers
   const [
     _galleryMobile,
